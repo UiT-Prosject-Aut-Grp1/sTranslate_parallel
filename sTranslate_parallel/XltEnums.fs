fb4640539299352cbb20970d5b3cae2cb9895d4a
akkarvik <akkarvik@hotmail.com>
2016-05-09 14:04:01 +0200

    Removed unneccesary junk

--- sTranslate_parallel/XltEnums.fs
+++ sTranslate_parallel/XltEnums.fs
@@ -8,32 +8,24 @@ module XltEnums =
         | Text    = 2
         | ToolTip = 3
         | Page    = 4
-    type Criterias =
+    type CriteriaTypes =
         | None      = 0
         | Exact     = 1
         | StartWith = 2
         | EndWith   = 3
         | Contains  = 4
 
-    // Helper function to give the first element of a sequence, if it contains something
-    let checkHead (s : seq<'a>) =
-        if Seq.isEmpty s then
-            None
-        else 
-            Some <| Seq.head s
-
     // Get enumeration state
-    let GetEnumState myType (value : string) =
+    let getEnumState myType (value : string) =
         
         // Filters a string array and finds the correct Enumeration
         Enum.GetNames(myType)
-        |> Seq.filter (fun x -> x.ToLower() = value.ToLower())
-        |> checkHead
+        |> Seq.tryFind (fun x -> x.ToLower() = value.ToLower())
 
     // Creates an object of type PropertyType from the input string
-    let ToPropertyType value = 
-        GetEnumState typeof<PropertyTypes> value
+    let toProperty value = 
+        getEnumState typeof<PropertyTypes> value
 
     // Creates an object of type Criterias from the input string
-    let ToCriteria value =
-        GetEnumState typeof<Criterias> value
+    let toCriteria value =
+        getEnumState typeof<CriteriaTypes> value
--- sTranslate_parallel/XltTool.fs
+++ sTranslate_parallel/XltTool.fs
@@ -15,18 +15,17 @@ module XltTool =
     // SQL Type Provider. Give it a dummy connection string for now to satisfy the compiler.
     type dbSchema = SqlDataConnection<ConnectionStringName = "dbConnectionString">
 
-    // A search to the database
+    // Record type containing a search to the database
     type Search = {
         Criteria : string
         FromText : string
         Property : string
         Context : string
-        ToLanguageCode : string
+        ToLang : string
     }
 
     // Record type so that we can cache the database to memory and not have the data context go out of scope
     type Translation = {
-        Id : int
         FromText : string
         ToText : string
         Context : string
@@ -36,9 +35,10 @@ module XltTool =
         ToLang : string
     }
 
+    let fromLang = "en"
+    
     // Copies the contents of a database row into a record type
     let toTranslation (xlt : dbSchema.ServiceTypes.Translation) = {
-        Id = xlt.Id
         FromText = xlt.FromText
         ToText = xlt.ToText
         Context = xlt.Context
@@ -48,61 +48,50 @@ module XltTool =
         ToLang = xlt.ToLang
     }
 
-    // Database only supports translating from english for now
-    let FromLanguageCode = "en"
-
     // Copies the database to memory
-    let GetTranslations =
-        use db = dbSchema.GetDataContext(Settings.ConnectionStrings.DbConnectionString)
+    let getTranslations =
+        let db = dbSchema.GetDataContext(Settings.ConnectionStrings.DbConnectionString)
         query {
             for row in db.Translation do 
                 select row
-        } |> Seq.toList |> List.map toTranslation 
- 
+        } |> Seq.toArray |> Array.map toTranslation
+
     // Returns the correct translation for the given search
-    let FindTranslation (criteria : string) (fromText : string) (property : string) (context : string) toLanguageCode =
+    let findTranslation (s : Search) =
         // If fromtext does not contain a word, return an empty string
-        if fromText.Trim() = "" then
-            ""
-        else
-            let criteriaOption = ToCriteria criteria
-            let propertyOption = ToPropertyType property
-            // If criteria or property was not found in enums, return the original string
-            if criteriaOption = None || propertyOption = None then
-                fromText
-            else
-                // If toLanguageCode is not valid, sets it to default "no"
-                let toLang =
-                    match toLanguageCode with
-                    | null | "" -> "no"
-                    | _ -> toLanguageCode
+        match s.FromText.Trim() with
+        | "" -> ""
+        | _ ->
+            
+            // If toLanguageCode is not valid, sets it to default "no"
+            let toLang =
+                match s.ToLang with
+                | null | "" -> "no"
+                | _ -> s.ToLang
 
-                // Get the database 
-                let collection = GetTranslations
-                
-                // Search for a valid translation
-                let result =
-                    collection
-                    |> Seq.filter (fun row -> row.Criteria.ToLower() = criteriaOption.Value.ToLower())
-                    |> Seq.filter (fun row -> row.FromLang = FromLanguageCode)
-                    |> Seq.filter (fun row -> row.FromText = fromText)
-                    |> Seq.filter (fun row -> row.Property.ToLower() = propertyOption.Value.ToLower())
-                    |> Seq.filter (fun row -> row.Context.ToLower() = context.ToLower())
-                    |> Seq.filter (fun row -> row.ToLang = toLang)
-                    |> checkHead
+            // Search for a valid translation
+            let result =
+                getTranslations
+                |> Array.tryFind ( fun row -> 
+                    row.Criteria.ToLower() = s.Criteria.ToLower() &&
+                    row.FromLang = fromLang &&
+                    row.FromText = s.FromText &&
+                    row.Property.ToLower() = s.Property.ToLower() &&
+                    row.Context.ToLower() = s.Context.ToLower() &&
+                    row.ToLang = toLang )
 
-                match result with
-                    | Some x -> x.ToText
-                    | None -> fromText
-    
+            match result with
+                | Some x -> x.ToText
+                | None -> s.FromText
+
     // Wraps FindTranslation inside an async
-    let FindTranslationAsync criteria fromText property context toLanguageCode = 
-        async { return FindTranslation criteria fromText property context toLanguageCode }
+    let findTranslationAsync s = 
+        async { return findTranslation s }
 
     // Takes a list of database searches, makes asyncs out of them, and combines them into a single parallel task.
     // Returns a list of results, that are in the same order as the searches.
-    let ToTextBatch (searchList : Search list) =
+    let getToTextAsync (searchList : Search list) =
         searchList
-        |> List.map (fun s -> FindTranslationAsync s.Criteria s.FromText s.Property s.Context s.ToLanguageCode)
+        |> List.map findTranslationAsync
         |> Async.Parallel
         |> Async.RunSynchronously
